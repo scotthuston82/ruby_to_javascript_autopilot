44962bacfadbfb6f67fa507973652276156e58dc
Scott <scotthuston1982@gmail.com>
2018-02-07 12:05:22 -0500

    finished

--- autopilot.js
+++ autopilot.js
@@ -1,5 +1,9 @@
-function getNewCar() {
+var getNewCar = {
   city: 'Toronto',
   passengers: 0,
   gas: 100
+}
+function addCar(cars, newCar) {
+  cars.push(newCar);
+  return "Adding new car to fleet. Fleet size is now" + cars.length ".";
 }
